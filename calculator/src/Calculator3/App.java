1a2d6168a2aeaa837d7d84e840c5f7badc794b32
K <xv40101392@gmail.com>
2024-05-04 21:11:10 +0900

    추가

--- calculator/src/Calculator3/App.java
+++ calculator/src/Calculator3/App.java
@@ -6,34 +6,41 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double result = 0;
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        double a = sc.nextDouble();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        for (int i = 1; i >= 0; i++) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            double a = sc.nextDouble();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        double b = sc.nextDouble();
-        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            double b = sc.nextDouble();
+            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String oper= sc.next();
-        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String oper= sc.next();
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
 
-        if(oper.equals("+")){
-            result = a+b;
+            if(oper.equals("+")){
+                result = a+b;
+            }
+            else if(oper.equals("-")){
+                result = a-b;
+            }
+            else if(oper.equals("*")){
+                result = a*b;
+            }
+            else if(oper.equals("/")){
+                result = a/b;
+            }
+
+            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+            System.out.println("결과: " + result);
+            System.out.print("더 계산하시겠습니까? (아무 키나 눌러주세요)(exit 입력 시 종료): ");
+            String onoff = sc.next();
+            if(onoff.equals("exit")){
+                break;
+            }
         }
-        else if(oper.equals("-")){
-            result = a-b;
-        }
-        else if(oper.equals("*")){
-            result = a*b;
-        }
-        else if(oper.equals("/")){
-            result = a/b;
-        }
 
-        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
-
-        System.out.println("결과: " + result);
 
     }
 }
