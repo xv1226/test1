c7a46a3bbb75975d472b2ae41401f43acee82b5e
K <xv40101392@gmail.com>
2024-05-06 14:41:14 +0900

    추가

--- calculator/src/Calculator3/App.java
+++ calculator/src/Calculator3/App.java
@@ -1,12 +1,16 @@
 package Calculator3;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double result = 0;
-        for (int i = 1; i >= 0; i++) {
+        ArrayList<Double> resultlist = new ArrayList<>();
+
+        for (int i = 0; i >= -1; i++) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             double a = sc.nextDouble();
             // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
@@ -31,16 +35,23 @@ public class App {
             else if(oper.equals("/")){
                 result = a/b;
             }
-
+            resultlist.add(result);
             /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
             System.out.println("결과: " + result);
+
+            int index= resultlist.indexOf(result);
+            System.out.println(index+"번째 값 저장 완료\n");
+
+            if(index==9){
+                resultlist.remove(0);
+            }
+
             System.out.print("더 계산하시겠습니까? (아무 키나 눌러주세요)(exit 입력 시 종료): ");
+
             String onoff = sc.next();
             if(onoff.equals("exit")){
                 break;
             }
         }
-
-
     }
 }
